e30bec7b2abf0be17ba256e2d89ec4e3aae72488
delisyp <sithlaw@gmail.com>
2023-03-04 02:07:33 +0900

    🚧 Implement deleting all search-history-items

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -1,6 +1,11 @@
 const gnbSearch = document.querySelector('.gnb-search')
 const gnbSearchInput = gnbSearch.querySelector('input')
 const gnbSearchHistory = gnbSearch.querySelector('.search-history')
+const gnbSearchHistoryList = gnbSearch.querySelector('ol')
+
+const deletAllButton = gnbSearchHistory.querySelector(
+  '.search-history-header button'
+)
 
 function closeGnbSearchHistoryOnClickingOutside(e) {
   if (!gnbSearch.contains(e.target)) {
@@ -10,6 +15,12 @@ function closeGnbSearchHistoryOnClickingOutside(e) {
 }
 
 function openGnbSearchHistory() {
+  // 체크  => gnbSearchHistoryList안에 Li가 몇개가 있는지
+  // li가 0개 => 실행 x
+  if (gnbSearchHistoryList.children.length === 0) {
+    return
+  }
+
   if (!gnbSearchHistory.classList.contains('is-active')) {
     window.addEventListener('click', closeGnbSearchHistoryOnClickingOutside)
   }
@@ -17,3 +28,11 @@ function openGnbSearchHistory() {
 }
 
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
+
+function deletAllSearchHistoryItems() {
+  // gnbSearchHistoryList 안에 들어있는 모든 li를 삭제해!
+  gnbSearchHistoryList.innerHTML = ''
+  gnbSearchHistory.classList.remove('is-active')
+}
+
+deletAllButton.addEventListener('click', deletAllSearchHistoryItems)
