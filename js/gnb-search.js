96c6acf7f777be616e2896f804b45e4546b13669
delisyp <sithlaw@gmail.com>
2023-03-04 11:41:32 +0900

    🚧 Implement deleting individual search-history-item

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -6,11 +6,18 @@ const gnbSearchHistoryList = gnbSearch.querySelector('ol')
 const deletAllButton = gnbSearchHistory.querySelector(
   '.search-history-header button'
 )
+const deleteButttonList =
+  gnbSearchHistoryList.querySelectorAll('.delete-button')
+
+function closeGnbSearchHistory() {
+  gnbSearchHistory.classList.remove('is-active')
+  window.removeEventListener('click', closeGnbSearchHistoryOnClickingOutside)
+}
 
 function closeGnbSearchHistoryOnClickingOutside(e) {
+  // *console.log('CLOSE!!', e.target)
   if (!gnbSearch.contains(e.target)) {
-    gnbSearchHistory.classList.remove('is-active')
-    window.removeEventListener('click', closeGnbSearchHistoryOnClickingOutside)
+    closeGnbSearchHistory()
   }
 }
 
@@ -32,7 +39,24 @@ gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
 function deletAllSearchHistoryItems() {
   // gnbSearchHistoryList 안에 들어있는 모든 li를 삭제해!
   gnbSearchHistoryList.innerHTML = ''
-  gnbSearchHistory.classList.remove('is-active')
+  closeGnbSearchHistory()
 }
 
 deletAllButton.addEventListener('click', deletAllSearchHistoryItems)
+
+function deletSearchHistoryItem(e) {
+  e.stopPropagation()
+  // li 삭제
+  // * console.log('DELETE!!')
+  const itemToDelete = this.parentNode
+  gnbSearchHistoryList.removeChild(itemToDelete)
+
+  if (gnbSearchHistoryList.children.length === 0) {
+    closeGnbSearchHistory()
+  }
+}
+
+deleteButttonList.forEach((button) => {
+  // * console.log(button)
+  button.addEventListener('click', deletSearchHistoryItem)
+})
